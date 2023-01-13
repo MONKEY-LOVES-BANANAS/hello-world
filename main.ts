4fb2bbd7335a23e2a2b7290ec314ef3840b0a5c0
MONKEY-LOVES-BANANAS <122555244+MONKEY-LOVES-BANANAS@users.noreply.github.com>
2023-01-13 12:25:31 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,6 +3,11 @@ input.onButtonPressed(Button.A, function () {
 })
 input.onButtonPressed(Button.AB, function () {
     basic.showString("MONKEY")
+    for (let index = 0; index < 4; index++) {
+        music.playMelody("C5 G A E E G C5 B ", 129)
+        music.setVolume(255)
+        music.ringTone(9100)
+    }
 })
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Chessboard)
