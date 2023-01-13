e052e8e87e75e5578b241c5aed34a4c0333b0464
MONKEY-LOVES-BANANAS <122555244+MONKEY-LOVES-BANANAS@users.noreply.github.com>
2023-01-13 12:10:29 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Heart)
 })
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("MONKEY")
+})
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Chessboard)
 })
